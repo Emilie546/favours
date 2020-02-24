d5dd1c702a26e6391ae9e4f81bcdb6710cec9337
Emilie Keller <emilie.keller@bluewin.ch>
2020-02-24 15:49:49 +0100

    methodes controllers

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,2 +1,32 @@
 class ContractsController < ApplicationController
+  def index
+    @contracts = Contract.all
+  end
+
+  def show
+    @contract = Contract.find(params[:id])
+  end
+
+  def new
+    @contract = Contract.new(params[:favour_id])
+    @contract = @favour.contract.build
+  end
+
+  def create
+    @contract = Contract.new(contract_params)
+    @favour = Favour.find(params[:favour_id])
+    @contract.user = current_user
+    @contract.favour = @favour
+    if @contract.save
+      redirect_to contracts_path
+    else
+      render 'favours/show'
+    end
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract).permit(:cancelled_at)
+  end
 end
--- app/controllers/favours_controller.rb
+++ app/controllers/favours_controller.rb
@@ -1,2 +1,47 @@
 class FavoursController < ApplicationController
+  def index
+    @favours = Favour.all
+  end
+
+  def show
+    @favour = Favour.find(params[:id])
+  end
+
+  def new
+    @favour = Favour.new
+  end
+
+  def create
+    #@favour = Favour.new(favour_params)
+    #@favour.user = current_user
+
+    @favour = current_user.favours.build(favour_params)
+    if @favour.save
+      redirect_to favour_path(@favour)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @favour = Favour.find(params[:id])
+  end
+
+  def update
+    @favour = Favour.find(params[:id])
+    @favour.update(favour_params)
+    redirect_to favour_path(@favour)
+  end
+
+  def destroy
+    @favour = Favour.find(params[:id])
+    @favour.destroy
+    redirect_to favours_path
+  end
+
+  private
+
+  def favour_params
+    params.require(:favour).permit(:name, :description, :price, :location, :start_time, :end_time)
+  end
 end
