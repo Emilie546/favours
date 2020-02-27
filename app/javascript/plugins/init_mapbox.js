81fd8b0a34e5f9414e0db5fdde5d72ae64d14c20
Emilie Keller <emilie.keller@bluewin.ch>
2020-02-27 14:48:52 +0100

    deux markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,6 +3,7 @@ import { setPosition } from './current_location'
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
+  const mapMarkers = [];
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -11,10 +12,13 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(map, markers);
+    addMarkersToMap(map, markers ,mapMarkers);
+
     fitMapToMarkers(map, markers);
     if (mapElement.dataset.page === "show") {
-      navigator.geolocation.watchPosition(function(position){succesPosition(markers, map, position)});
+      navigator.geolocation.watchPosition((position) => {
+         succesPosition(markers, map, position, mapMarkers)
+      });
     }
   }
 };
@@ -25,34 +29,46 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
-const succesPosition = (markers, map, pos) => {
+const succesPosition = (markers, map, pos, mapMarkers) => {
   const mapElement = document.getElementById('map');
   const lng = pos.coords.longitude;
   const lat = pos.coords.latitude;
   const marker_user = {lng: lng, lat: lat};
-  addMypositionToMap(map, marker_user);
-  markers.push(marker_user);
-  fitMapToMarkers(map, markers);
+
+  mapMarkers.forEach((m) => {
+    m.remove();
+  });
+  mapMarkers = [];
+  console.log(markers)
+  addMarkersToMap(map,markers,mapMarkers);
+  addMypositionToMap(map, marker_user, mapMarkers);
+
+  fitMapToMarkers(map, markers.concat([marker_user]));
 };
 
-const addMypositionToMap = (map, marker) => {
+const addMypositionToMap = (map, marker, mapMarkers) => {
 
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
-    new mapboxgl.Marker({color: 'red'})
+    const mapMarker = new mapboxgl.Marker({color: 'red'})
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
       .addTo(map);
+    mapMarkers.push(mapMarker);
+
 };
 
-const addMarkersToMap = (map, markers) => {
+const addMarkersToMap = (map, markers, mapMarkers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
-    new mapboxgl.Marker()
+    const mapMarker = new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
       .addTo(map);
+
+    mapMarkers.push(mapMarker);
+
   });
 };
 
