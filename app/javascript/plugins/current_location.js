3ee1e6cea53d4bf446c08d1c138e5fe68b94c752
Emilie Keller <emilie.keller@bluewin.ch>
2020-02-27 14:02:54 +0100

    map avec marker current location

--- app/javascript/plugins/current_location.js
+++ app/javascript/plugins/current_location.js
@@ -1,12 +1,51 @@
-const setPosition = () => {
+export const setPosition = () => {
   const position = navigator.geolocation.watchPosition(succesPosition);
+
 }
 
 const succesPosition = (pos) => {
-
+  const mapElement = document.getElementById('map');
+  const lng = pos.coords.longitude;
+  const lat = pos.coords.latitude;
+  const marker_user = [lng, lat];
+  console.log(marker_user)
+  // marker_user.push(JSON.parse(mapElement.dataset.marker_user));
+  // console.log(marker_user);
 };
 
 const setMarker = () => {
   const button = document.querySelector('#location');
   button.addEventListener('click', sendPosition);
 };
+
+
+// const sendPosition = () => {
+//   const position = navigator.geolocation.getCurrentPosition(succesPosition);
+// }
+// const succesPosition = (pos) => {
+//   const button = document.querySelector('#location');
+//   const csrfToken = document.querySelector("[name='csrf-token']").content;
+//   fetch(button.dataset.path, {
+//     method: "POST",
+//     headers: {
+//       'X-CSRF-Token': csrfToken,
+//       'Content-Type': 'application/json'
+//     },
+//     credentials: "same-origin",
+//     body: JSON.stringify({
+//       alert: {
+//         latitude: pos.coords.latitude,
+//         longitude : pos.coords.longitude
+//       }
+//     })
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       window.location.href = data.path;
+//     });
+// };
+// const initSendPosition = () => {
+//   const button = document.querySelector('#location');
+//   button.addEventListener('click', sendPosition);
+// };
+// export { initSendPosition };
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,13 +1,8 @@
 import mapboxgl from 'mapbox-gl';
+import { setPosition } from './current_location'
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
-  const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-};
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -16,20 +11,39 @@ const initMapbox = () => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
-    // const markers = [];
-    // markers.push(JSON.parse(mapElement.dataset.markers));
-    // console.log(markers);
-    markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
-    });
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+    if (mapElement.dataset.page === "show") {
+      navigator.geolocation.watchPosition(function(position){succesPosition(markers, map, position)});
+    }
   }
 };
 
-export { initMapbox }
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const succesPosition = (markers, map, pos) => {
+  const mapElement = document.getElementById('map');
+  const lng = pos.coords.longitude;
+  const lat = pos.coords.latitude;
+  const marker_user = {lng: lng, lat: lat};
+  addMypositionToMap(map, marker_user);
+  markers.push(marker_user);
+  fitMapToMarkers(map, markers);
+};
+
+const addMypositionToMap = (map, marker) => {
+
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+    new mapboxgl.Marker({color: 'red'})
+      .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
+      .addTo(map);
+};
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
@@ -41,3 +55,5 @@ const addMarkersToMap = (map, markers) => {
       .addTo(map);
   });
 };
+
+export { initMapbox }
