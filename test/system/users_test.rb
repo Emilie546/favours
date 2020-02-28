af1bbdd62f9c628078523f24331715d08b5f7aef
hugo mori <mori.hugo@outlook.com>
2020-02-28 15:49:32 +0100

    ok

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,8 +1,17 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  test "login user" do
-    visit "/"
-    login_as users(:george)
+  test "create user" do
+    visit "/users/sign_up"
+
+    fill_in 'user_first_name', with: 'Hugo'
+    fill_in 'user_last_name', with: 'Troxler'
+    fill_in 'user_email', with: 'hugo.troxler@outlook.com'
+    fill_in 'user_password', with: 'Jgaviphmy4'
+    fill_in 'user_password_confirmation', with: 'Jgaviphmy4'
+
+    click_on 'Sign up'
+
+    assert_equal root_path, page.current_path
   end
 end
